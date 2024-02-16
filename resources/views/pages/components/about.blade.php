e6b7f5fe58a1eab67d14907fa183dacdd2559caf
Ex1tar <96546619+ExPressOnePro@users.noreply.github.com>
2024-02-16 17:49:24 +0200

    16.02.2024 16:08

--- resources/views/pages/components/about.blade.php
+++ resources/views/pages/components/about.blade.php
@@ -1,58 +1,37 @@
 
-<div class="intro-contact">
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-4 heading-section text-center ftco-animate fadeInUp ftco-animated">
-                <h2 class="mb-4">CONTACTAŢI-NE</h2>
-            </div>
-        </div>
+<div class="container px-4 py-5">
+<h2 class="text-center mb-4">CONTACTAŢI-NE</h2>
+<div class="row g-4 py-5 row-cols-1 row-cols-lg-3">
 
-        <div class="row justify-content-center mb-5">
-            <div class="col-md-4 p-4">
-                <div class="text-center">
-                    <div class="icon d-flex align-items-center justify-content-center">
-                        <i class="fa-solid fa-location-dot"></i>
-                    </div>
-                    <div>
-                        <h3 class="mb-4">Adresa</h3>
-                        <h6>MD-3645 Republica Moldova, r.Ungheni, s.Semeni</h6>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-4 p-4 text-center align-items-center">
-                <div>
-                    <div class="icon d-flex align-items-center justify-content-center">
-                        <i class="fa-solid fa-phone"></i>
-                    </div>
-                    <div>
-                        <h3 class="mb-4">Numar de contact</h3>
-                        <h5><a href="tel://023670276">(0236) 70276</a></h5>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-4 p-4">
-                <div class="text-center">
-                    <div class="icon d-flex align-items-center justify-content-center">
-                        <i class="fa-solid fa-at"></i>
-                    </div>
-                    <div>
-                        <h3 class="mb-4">Email Address</h3>
-                        <h5><a href="">gimnaziulsemeni@gmail.com</a></h5>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div id="contact" class="contact text-center">
-            <div class="container">
-                <ul>
-                    <li>
-                        <a class="text-center align-items-center" href="https://www.facebook.com/people/Gimnaziul-Semeni/100080282674422/?paipv=0&eav=AfaMRwHR-_hLKHoPO9P_bbGyGpDDZHlLYaVMBuSd5K5s9aw59lfg7o_K_EvMgUaOT08&_rdr">
-                            <i class="fa fa-facebook"></i> Facebook
-                        </a>
-                    </li>
-                </ul>
-            </div>
-        </div>
 
+    <div class="col text-center py-3">
+        <div class="icon-square text-body-emphasis bg-body-secondary d-inline-flex align-items-center justify-content-center fs-4 flex-shrink-0 me-3">
+            <i class="fa fa-location-dot"></i>
+        </div>
+        <div>
+            <h3 class="fs-2 text-body-emphasis mb-4">Adresa</h3>
+            <h6>MD-3645 Republica Moldova, r.Ungheni, s.Semeni</h6>
+        </div>
     </div>
+
+    <div class="col text-center py-3">
+        <div class="icon-square text-body-emphasis bg-body-secondary d-inline-flex align-items-center justify-content-center fs-4 flex-shrink-0 me-3">
+            <i class="fa fa-location-dot"></i>
+        </div>
+        <div>
+            <h3 class="fs-2 text-body-emphasis mb-4">Numar de contact</h3>
+            <a href="tel://023670276"><h4>(0236) 70276</h4></a>
+        </div>
+    </div>
+
+    <div class="col text-center py-3">
+        <div class="icon-square text-body-emphasis bg-body-secondary d-inline-flex align-items-center justify-content-center fs-4 flex-shrink-0 me-3">
+            <i class="fa-solid fa-at"></i>
+        </div>
+        <div>
+            <h3 class="fs-2 text-body-emphasis mb-4">Email Address</h3>
+            <h4>gimnaziulsemeni@gmail.com</h4>
+        </div>
+    </div>
+</div>
 </div>
